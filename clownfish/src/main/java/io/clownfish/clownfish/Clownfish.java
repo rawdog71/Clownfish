07c5cb7313dac6dba223182bf857514253e461c8
rawdog <rawdog@arcor.de>
2021-09-23 13:21:43 +0200

    Direct template call deleted
    Secure issue. It's not possible to call a Freemarker or Velocity template via URL

--- clownfish/src/main/java/io/clownfish/clownfish/Clownfish.java
+++ clownfish/src/main/java/io/clownfish/clownfish/Clownfish.java
@@ -642,7 +642,7 @@ public class Clownfish {
         }
     }
     
-    @GetMapping(path = "/{name}.tpl")
+    @GetMapping(path = {"/{name}.tpl", "/{name}.html"})
     public void universalGetTemplate(@PathVariable("name") String name, @Context HttpServletRequest request, @Context HttpServletResponse response) {
         CfTemplate cftemplate = null;
         try {
@@ -654,42 +654,6 @@ public class Clownfish {
                 outwriter.println(cftemplate.getContent());
             } else {
                 LOGGER.warn("ONLY HTML Templates");
-            }
-        } catch (Exception ex) {
-            LOGGER.warn("Template NOT FOUND");
-        }
-    }
-    
-    @GetMapping(path = "/{name}.ftl")
-    public void universalGetFreemarkerTemplate(@PathVariable("name") String name, @Context HttpServletRequest request, @Context HttpServletResponse response) {
-        CfTemplate cftemplate = null;
-        try {
-            cftemplate = cftemplateService.findByName(name);
-            if (0 == cftemplate.getScriptlanguage()) {
-                response.setContentType("text/html");
-                response.setCharacterEncoding("UTF-8");
-                PrintWriter outwriter = response.getWriter();
-                outwriter.println(cftemplate.getContent());
-            } else {
-                LOGGER.warn("ONLY Freemarker Templates");
-            }
-        } catch (Exception ex) {
-            LOGGER.warn("Template NOT FOUND");
-        }
-    }
-    
-    @GetMapping(path = "/{name}.vm")
-    public void universalGetVelocityTemplate(@PathVariable("name") String name, @Context HttpServletRequest request, @Context HttpServletResponse response) {
-        CfTemplate cftemplate = null;
-        try {
-            cftemplate = cftemplateService.findByName(name);
-            if (1 == cftemplate.getScriptlanguage()) {
-                response.setContentType("text/html");
-                response.setCharacterEncoding("UTF-8");
-                PrintWriter outwriter = response.getWriter();
-                outwriter.println(cftemplate.getContent());
-            } else {
-                LOGGER.warn("ONLY Velocity Templates");
             }
         } catch (Exception ex) {
             LOGGER.warn("Template NOT FOUND");
