bacd18213c7caf0b535e94bf9efbd5026af2b3fc
Rainer Sulzbach <rawdog@arcor.de>
2021-11-19 17:05:48 +0100

    Update sql-bootstrap_0_5_1.sql
    added table creation for cf_java + cf_javaversion

--- clownfish/sql-bootstrap_0_5_1.sql
+++ clownfish/sql-bootstrap_0_5_1.sql
@@ -1,6 +1,36 @@
 USE clownfish;
 
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `cf_javascript`
+--
+
+CREATE TABLE IF NOT EXISTS `cf_java` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) NOT NULL,
+  `content` mediumtext,
+  `checkedoutby` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `cf_javascriptversion`
+--
+
+CREATE TABLE IF NOT EXISTS `cf_javaversion` (
+  `java` bigint(20) unsigned NOT NULL,
+  `version` bigint(20) NOT NULL,
+  `content` blob,
+  `tstamp` datetime NOT NULL,
+  `commitedby` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`java`,`version`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 INSERT INTO `cf_property` (`hashkey`, `value`, `nodelete`) VALUES
 ('folder_maven', '', 1);
+
 
 COMMIT;
