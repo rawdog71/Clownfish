1347a838bac0336723f57718058e0242d421d5eb
Rainer Sulzbach <rawdog@arcor.de>
2022-02-10 15:21:54 +0100

    Update RestDatalist.java
    added rest service /deletedatalist

--- clownfish/src/main/java/io/clownfish/clownfish/rest/RestDatalist.java
+++ clownfish/src/main/java/io/clownfish/clownfish/rest/RestDatalist.java
@@ -18,11 +18,15 @@ package io.clownfish.clownfish.rest;
 import io.clownfish.clownfish.datamodels.AuthTokenList;
 import io.clownfish.clownfish.datamodels.RestDatalistParameter;
 import io.clownfish.clownfish.dbentities.CfClass;
+import io.clownfish.clownfish.dbentities.CfClasscontent;
 import io.clownfish.clownfish.dbentities.CfList;
+import io.clownfish.clownfish.dbentities.CfListcontent;
 import io.clownfish.clownfish.serviceinterface.CfClassService;
 import io.clownfish.clownfish.serviceinterface.CfListService;
+import io.clownfish.clownfish.serviceinterface.CfListcontentService;
 import io.clownfish.clownfish.utils.ApiKeyUtil;
 import io.clownfish.clownfish.utils.FolderUtil;
+import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -38,6 +42,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class RestDatalist {
     @Autowired transient CfClassService cfclassService;
     @Autowired transient CfListService cflistService;
+    @Autowired transient CfListcontentService cflistcontentService;
     @Autowired FolderUtil folderUtil;
     @Autowired ApiKeyUtil apikeyutil;
     @Autowired transient AuthTokenList authtokenlist;
@@ -67,6 +72,40 @@ public class RestDatalist {
                         CfList newlist2 = cflistService.create(newlist);
                         idp.setListid(newlist2.getId());
                         idp.setReturncode("OK");
+                    }
+                } else {
+                    idp.setReturncode("Wrong API KEY");
+                }
+            } else {
+                idp.setReturncode("Invalid token");
+            }
+        } catch (javax.persistence.NoResultException ex) {
+            idp.setReturncode("NoResultException");
+        }
+        return idp;
+    }
+    
+    @PostMapping("/deletedatalist")
+    public RestDatalistParameter restDeleteDatalist(@RequestBody RestDatalistParameter idp) {
+        return deleteDatalist(idp);
+    }
+    
+    private RestDatalistParameter deleteDatalist(RestDatalistParameter idp) {
+        try {
+            String token = idp.getToken();
+            if (authtokenlist.checkValidToken(token)) {
+                String apikey = idp.getApikey();
+                if (apikeyutil.checkApiKey(apikey, "InsertDatalist")) {
+                    try {
+                        CfList list = cflistService.findByName(idp.getListname());
+                        List<CfListcontent> listcontentList = cflistcontentService.findByListref(list.getId());
+                        for (CfListcontent listcontent : listcontentList) {
+                            cflistcontentService.delete(listcontent);
+                        }
+                        cflistService.delete(list);
+                        idp.setReturncode("OK");
+                    } catch (javax.persistence.NoResultException ex) {
+                        idp.setReturncode("Datalist not found");
                     }
                 } else {
                     idp.setReturncode("Wrong API KEY");
