36a733118dd395a1248dd90f5421a3988148fc04
rawdog <rawdog@arcor.de>
2022-09-21 14:53:04 +0200

    Create CORSFilter.java

---
+++ clownfish/src/main/java/io/clownfish/clownfish/CORSFilter.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2022 SulzbachR.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.clownfish.clownfish;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author SulzbachR
+ */
+@WebFilter(asyncSupported = true, urlPatterns = { "/*" })
+public class CORSFilter implements Filter {
+    
+    private static final String[] allowedOrigins = {
+            "http://localhost:4200"
+    };
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        
+        String requestOrigin = request.getHeader("Origin");
+        if(isAllowedOrigin(requestOrigin)) {
+            // Authorize the origin, all headers, and all methods
+            ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Origin", requestOrigin);
+            ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Headers", "*");
+            ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Methods",
+                    "GET, OPTIONS, HEAD, PUT, POST, DELETE");
+
+            HttpServletResponse resp = (HttpServletResponse) servletResponse;
+
+            // CORS handshake (pre-flight request)
+            if (request.getMethod().equals("OPTIONS")) {
+                resp.setStatus(HttpServletResponse.SC_ACCEPTED);
+                return;
+            }
+        }
+        // pass the request along the filter chain
+        filterChain.doFilter(request, servletResponse);
+    }
+    
+    private boolean isAllowedOrigin(String origin){
+        return true;
+        /*
+        for (String allowedOrigin : allowedOrigins) {
+            if(origin.equals(allowedOrigin)) return true;
+        }
+        return false;
+        */
+    }
+    
+}
