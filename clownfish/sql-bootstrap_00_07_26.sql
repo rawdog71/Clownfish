99766072bbad4d05effbb929a34d4d55a25bce30
rawdog <rawdog@arcor.de>
2023-05-30 10:45:25 +0200

    minor fixes

--- clownfish/sql-bootstrap_00_07_26.sql
+++ clownfish/sql-bootstrap_00_07_26.sql
@@ -1,11 +1,11 @@
 USE clownfish;
 
+ALTER TABLE `clownfish`.`cf_site` 
+ADD COLUMN `offline` TINYINT(1) NULL DEFAULT '0' AFTER `loginsite`;
+
 CREATE TABLE `clownfish`.`cf_staticsite` (
   `id` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
   `site` VARCHAR(64) NULL,
   `urlparams` VARCHAR(1024) NULL,
   `tstamp` DATETIME NOT NULL,
   PRIMARY KEY (`id`));
-
-ALTER TABLE `clownfish`.`cf_site` 
-ADD COLUMN `offline` TINYINT(1) NULL DEFAULT '0' AFTER `loginsite`;
