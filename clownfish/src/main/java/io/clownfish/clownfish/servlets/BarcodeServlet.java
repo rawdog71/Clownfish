46ab740afe925455a3e95222ca65e67da4018b75
rawdog <rawdog@arcor.de>
2023-10-11 10:06:32 +0200

    added UbuntuMono font

--- clownfish/src/main/java/io/clownfish/clownfish/servlets/BarcodeServlet.java
+++ clownfish/src/main/java/io/clownfish/clownfish/servlets/BarcodeServlet.java
@@ -26,7 +26,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.util.logging.Level;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import javax.imageio.ImageIO;
