f4c30ff19c27e03aec3b10fa58328817f20191a9
rawdog <rawdog@arcor.de>
2024-05-24 13:12:11 +0200

    fixed datetime bug with OData and CFAttribute

--- clownfish/src/main/java/io/clownfish/clownfish/odata/EntityUtil.java
+++ clownfish/src/main/java/io/clownfish/clownfish/odata/EntityUtil.java
@@ -376,11 +376,6 @@ public class EntityUtil {
                                 Property prop = entity.getProperty(attribut.getName());
                                 if (null != prop) {
                                     newcontent = contentUtil.setAttributValue(newcontent, entity.getProperty(attribut.getName()).getValue().toString());
-                                    if (0 != attribut.getAttributetype().getName().compareToIgnoreCase("datetime")) {
-                                        newcontent = contentUtil.setAttributValue(newcontent, entity.getProperty(attribut.getName()).getValue().toString());
-                                    } else {
-                                        newcontent = contentUtil.setAttributValue(newcontent, ((GregorianCalendar)entity.getProperty(attribut.getName()).getValue()).toZonedDateTime().format(DateTimeFormatter.ofPattern("dd.MM.yyyy")));
-                                    }
                                 } else {
                                     newcontent = contentUtil.setAttributValue(newcontent, null);
                                 }
