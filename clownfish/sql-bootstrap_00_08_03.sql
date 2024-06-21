3c215fc6167422e0440c02990c34300b7c32caa8
rawdog <rawdog@arcor.de>
2024-06-21 10:37:14 +0200

    Update sql-bootstrap_00_08_03.sql
    fixed default 0
    added update for setting the correct loginsiteref to the loginsite

--- clownfish/sql-bootstrap_00_08_03.sql
+++ clownfish/sql-bootstrap_00_08_03.sql
@@ -2,7 +2,9 @@ USE clownfish;
 
 ALTER TABLE `clownfish`.`cf_site`
 ADD COLUMN `isloginsite` TINYINT(1) NULL DEFAULT 0 AFTER `invisible`,
-ADD COLUMN `loginsiteref` BIGINT(20) NULL DEFAULT 0 AFTER `isloginsite`;
+ADD COLUMN `loginsiteref` BIGINT(20) NULL DEFAULT NULL AFTER `isloginsite`;
 
 ALTER TABLE `clownfish`.`cf_class`
 ADD COLUMN `loginclass` TINYINT(1) NULL DEFAULT 0 AFTER `encrypted`;
+
+UPDATE `clownfish`.`cf_site` SET `loginsiteref` = (SELECT t.id FROM (SELECT id, name FROM `clownfish`.`cf_site`) t WHERE t.`name` = `loginsite`)
